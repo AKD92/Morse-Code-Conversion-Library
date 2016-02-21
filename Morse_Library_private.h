16b7b014fb13cad7e9fa1fbea50dfe5caf0c8b14
Ashis Kumar Das <akd.bracu@gmail.com>
2016-02-21 23:15:39 +0600

    Added files via upload

---
+++ Morse_Library_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef MORSE_LIBRARY_PRIVATE_H
+#define MORSE_LIBRARY_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.1"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	"akd.bracu@gmail.com"
+#define FILE_VERSION	"1.0.0.1"
+#define FILE_DESCRIPTION	"Morse Code Manipulation Library"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Ashis Kumar Das"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"Morse Code Manipulation Library"
+#define PRODUCT_VERSION	"1.0.0.1"
+
+#endif /*MORSE_LIBRARY_PRIVATE_H*/
